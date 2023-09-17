d44fa0ea55d331fc55ad40f8ba027a11077e89ca
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-17 03:10:31 +0200

    iterere animasjoner med space

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js'
 
 //custom imports
 import { sendError } from './errorHandler.js'
@@ -19,7 +21,7 @@ function main() {
   const near = 0.1
   const far = 100
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  camera.position.set(0, 10, 20)
+  camera.position.set(0, 20, 40)
 
   //controls
   const controls = new OrbitControls(camera, canvas)
@@ -28,16 +30,128 @@ function main() {
 
   //scene
   const scene = new THREE.Scene()
-  scene.background = new THREE.Color('black')
+  scene.background = new THREE.Color('white')
 
-  /*
-  
-  innhold
-  
-  */
+  function addLight(...pos) {
+    const color = 0xffffff
+    const intensity = 2.5
+    const light = new THREE.DirectionalLight(color, intensity)
+    light.position.set(...pos)
+    scene.add(light)
+    scene.add(light.target)
+  }
+
+  addLight(5, 5, 2)
+  addLight(-5, 5, 5)
+
+  //loader
+
+  const manager = new THREE.LoadingManager()
+  manager.onLoad = init
+
+  const progressbarElem = document.querySelector('#progressbar')
+  manager.onProgress = (url, itemsLoaded, itemsTotal) => {
+    progressbarElem.computedStyleMap.width = `${
+      ((itemsLoaded / itemsTotal) * 100) | 0
+    }%`
+  }
+
+  const models = {
+    alienBug: { url: 'resources/models/alien-bug/alien-bug.glb' },
+  }
+  {
+    const gltfLoader = new GLTFLoader(manager)
+    for (const model of Object.values(models)) {
+      gltfLoader.load(model.url, (gltf) => {
+        model.gltf = gltf
+      })
+    }
+  }
+
+  function prepModelsAndAnimations() {
+    //
+    Object.values(models).forEach((model) => {
+      //
+      const animsByName = {}
+      model.gltf.animations.forEach((clip) => {
+        //
+        animsByName[clip.name] = clip
+        //
+      })
+      model.animations = animsByName
+    })
+  }
+
+  const mixers = []
+  const mixerInfos = []
+
+  function init() {
+    //
+    const loadingElem = document.querySelector('#loading')
+    loadingElem.style.display = 'none'
+
+    prepModelsAndAnimations()
+
+    Object.values(models).forEach((model, ndx) => {
+      const objectScale = 13
+      const clonedScene = SkeletonUtils.clone(model.gltf.scene)
+      const root = new THREE.Object3D()
+      root.add(clonedScene)
+      scene.add(root)
+
+      root.scale.set(objectScale, objectScale, objectScale) // Set the scale of the object
+
+      root.position.x = ndx
+      root.position.y = ndx + 5
+      root.position.z = ndx + 10
+
+      root.rotation.y = Math.PI / 4
+
+      const mixer = new THREE.AnimationMixer(clonedScene)
+      //const firstClip = Object.values(model.animations)[2]
+      // const action = mixer.clipAction(firstClip)
+      const actions = Object.values(model.animations).map((clip) => {
+        return mixer.clipAction(clip)
+      })
+      const mixerInfo = {
+        mixer,
+        actions,
+        actionNdx: -1,
+      }
+      ///   console.log(mixerInfo)
+      mixerInfos.push(mixerInfo)
+      playNextAction(mixerInfo)
+      //  action.play()
+      //   mixers.push(mixer)
+    })
+  }
+
+  function playNextAction(mixerInfo) {
+    const { actions, actionNdx } = mixerInfo
+    const nextActionNdx = (actionNdx + 1) % actions.length
+    mixerInfo.actionNdx = nextActionNdx
+    actions.forEach((action, ndx) => {
+      const enabled = ndx === nextActionNdx
+      action.enabled = enabled
+      if (enabled) {
+        action.play()
+      }
+    })
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.code == 'Space') {
+      // This checks for both lowercase and uppercase 'E'
+      console.log('E key pressed')
+
+      mixerInfos.forEach((mixerInfo) => {
+        playNextAction(mixerInfo)
+      })
+    }
+  })
 
   //render functinons
- function resizeRendererToDisplaySize(renderer) {
+  function resizeRendererToDisplaySize(renderer) {
     //
     const canvas = renderer.domElement
     const width = canvas.clientWidth
@@ -49,13 +163,25 @@ function main() {
     }
     return needResize
   }
-  function render() {
+
+  let then = 0
+  function render(now) {
+    //
+    now *= 0.001
+    const deltaTime = now - then
+    then = now
+    //
     if (resizeRendererToDisplaySize(renderer)) {
       //
       const canvas = renderer.domElement
       camera.aspect = canvas.clientWidth / canvas.clientHeight
       camera.updateProjectionMatrix()
     }
+
+    for (const { mixer } of mixerInfos) {
+      mixer.update(deltaTime)
+    }
+
     renderer.render(scene, camera)
 
     requestAnimationFrame(render)
