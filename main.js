aea38d07528ed74cc69f2c4c7004812ad7293cfc
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-17 12:50:52 +0200

    upadte

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function main() {
   }
 
   const models = {
-    alienBug: { url: 'public/models/alien-bug/alien-bug.glb' },
+    alienBug: { url: 'http://public.tomhaakon.com/public/alien-bug.glb' },
   }
   {
     const gltfLoader = new GLTFLoader(manager)
