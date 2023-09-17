69835ab6bd1dd4609240bb91afbe87c1b73816ce
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-17 13:32:05 +0200

    asdasddas

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function main() {
   }
 
   const models = {
-    alienBug: { url: 'resources/models/alien-bug/alien-bug.glb' },
+    alienBug: { url: 'dist/resources/models/alien-bug/alien-bug.glb' },
   }
   {
     const gltfLoader = new GLTFLoader(manager)
