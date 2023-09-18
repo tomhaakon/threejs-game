aaf486190906865565a37a1bc9b2ba55e60c43c7
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-09-18 12:04:28 +0200

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
+// const express = require("express");
+
+// const app = express();
+// app.use(express.json());
+
+// const API_URL = process.env.API_URL;
+// const API_KEY = process.env.API_KEY;
+
+// const port = 3000;
+
+// app.post("/api/add-text", (req, res) => {
+//   const { text } = req.body;
+//   // Insert the text into the database
+//   // Send a response
+//   res.json({ success: true, message: "Text added successfully" });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server);
 
-const API_URL = process.env.API_URL;
-const API_KEY = process.env.API_KEY;
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-const port = 3000;
+  // Example: Listen for a 'message' event from the client
+  socket.on("message", (data) => {
+    console.log(data);
 
-app.post("/api/add-text", (req, res) => {
-  const { text } = req.body;
-  // Insert the text into the database
-  // Send a response
-  res.json({ success: true, message: "Text added successfully" });
+    // Broadcast the message to all connected clients
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
