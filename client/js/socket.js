10bf44ba7e5a46259f3eecf70cc8c0ac1470cfad
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-09-18 13:07:25 +0200

    update

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,17 +1,19 @@
 import { io } from "socket.io-client";
-
+import { sendError } from "../errorHandler";
 // Assuming you've already imported and initialized socket.io on the client:
 
 const socket = io("https://threejs-game.onrender.com");
-
+let countNumber = 0;
 socket.on("connect", () => {
   console.log("Connected to the server");
 });
 // Assuming you've already established a socket connection to the server
+
 socket.on("updateUserCount", (count) => {
-  const userCountElement = document.getElementById("userCount");
-  userCountElement.textContent = count;
+  const userCountBox = document.getElementById("userCountBox");
+  userCountBox.textContent = "Users connected: " + count;
 });
+
 // Listen for the 'hello' event
 socket.on("hello", (message) => {
   console.log(message); // Should log "Hello from the server!"
