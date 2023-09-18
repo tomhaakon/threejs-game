cea60944e8ff45103486e878a2510cf07c1ab80b
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-09-18 12:33:24 +0200

    asdd

--- server/server.js
+++ server/server.js
@@ -22,10 +22,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use((req, res, next) => {
-  console.log("Origin Header:", req.headers.origin);
-  next();
-});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
