ade0866fc4ad91f60eba64b8e15efafa4dec0d2b
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-09-18 13:11:40 +0200

    asd

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use((req, res, next) => {
   const allowedOrigins = [
     "https://threejs-game.onrender.com",
+    "https://game.tomhaakon.com",
     "http://localhost:5173",
   ];
   const origin = req.headers.origin;
