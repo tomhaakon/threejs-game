0914bbe3af441129101762129df6fc4eb87b240f
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-21 01:59:11 +0200

    remove

--- client/js/controls/touchControls.js
+++ client/js/controls/touchControls.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import * as detectIt from 'detect-it'
-import nipplejs from 'nipplejs'
+
 import * as kd from 'keydrown'
 import { RotationHandler } from './touchRotation'
 import { MovementHandler } from './touchMovement'
